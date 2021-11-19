427a38cb0f637a2adbc340f139c634b7153b85f2
austriajimcaesar <201810807@gordoncollege.edu.ph>
2021-11-19 17:17:18 +0800

    admin dashboard

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
+  
 })
 export class SignupComponent implements OnInit {
 
